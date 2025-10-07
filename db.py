f19a44587aa4ec27cb646ccb6f43ca6532604c7c
Stiven Posada <esposada89@ucatolica.edu.co>
2025-10-07 15:49:43 -0500

    Refactor database connection to use environment variables for PostgreSQL configuration

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Depends
 from sqlmodel import SQLModel, create_engine, Session
 from typing import Annotated
+from dotenv import load_dotenv
+import os
 
-# Datos de conexión a PostgreSQL
-db_user = "postgres"
-db_password = "3621518"
-db_host = "localhost"
-db_port = ("5433")
-db_name = "GolAGol"
+load_dotenv()  # Carga el archivo .env
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
 
 # URL de conexión PostgreSQL
 db_url = f"postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
